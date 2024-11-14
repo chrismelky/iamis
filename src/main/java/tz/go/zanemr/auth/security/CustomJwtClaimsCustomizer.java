a35a285a9821f0661f702fb6c2115588e64a6792
Chris <chrismelky05@gmail.com>
2024-11-14 12:36:57 +0300

    fix auth token

--- src/main/java/tz/go/zanemr/auth/security/CustomJwtClaimsCustomizer.java
+++ src/main/java/tz/go/zanemr/auth/security/CustomJwtClaimsCustomizer.java
@@ -46,8 +46,8 @@ public class CustomJwtClaimsCustomizer implements OAuth2TokenCustomizer<JwtEncod
             customClaims.put("middleName", user.getMiddleName());
             customClaims.put("facilityCode", user.getFacilityCode());
             customClaims.put("roleIds", user.getRoles().stream().map(r->r.getId().toString()).toList());
-            customClaims.put("authorities", new ArrayList<>());
-//            customClaims.put("authorities", principal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
+//            customClaims.put("authorities", new ArrayList<>());
+            customClaims.put("authorities", principal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
             context.getClaims().claims(claims -> claims.putAll(customClaims));
         }
     }
