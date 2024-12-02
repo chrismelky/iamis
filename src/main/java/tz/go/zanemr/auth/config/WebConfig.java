5b6031b01a4fec6ed9b168052fffba5fdce6820b
Chris <chrismelky05@gmail.com>
2024-12-02 18:05:19 +0300

    diable ssl

--- src/main/java/tz/go/zanemr/auth/config/WebConfig.java
+++ src/main/java/tz/go/zanemr/auth/config/WebConfig.java
@@ -3,7 +3,6 @@ package tz.go.zanemr.auth.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import tz.go.zanemr.auth.security.CustomAuthorizationInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
