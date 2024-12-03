dce8b4c5ae9d57a21e9f7a3031f8be04d35e4857
Chris <chrismelky05@gmail.com>
2024-12-03 10:42:15 +0300

    change image matcher

--- src/main/java/tz/go/zanemr/auth/config/SecurityConfiguration.java
+++ src/main/java/tz/go/zanemr/auth/config/SecurityConfiguration.java
@@ -134,11 +134,11 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(HttpMethod.OPTIONS, "/**")
                         .permitAll() // Allow CORS preflight
-                        .requestMatchers("/login").permitAll()
-                        .requestMatchers("favicon.ico").permitAll()
-                        .requestMatchers("cover.png").permitAll()
-                        .requestMatchers("doctor.png").permitAll()
-                        .requestMatchers("smz.png").permitAll()
+                        .requestMatchers("login").permitAll()
+                        .requestMatchers("auth/favicon.ico").permitAll()
+                        .requestMatchers("auth/cover.png").permitAll()
+                        .requestMatchers("auth/doctor.png").permitAll()
+                        .requestMatchers("auth/smz.png").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
 
