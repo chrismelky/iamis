6ae318a2a972dc49158977433734113e2cad3c5b
MasterBori <maulidboriussi@gmail.com>
2025-01-01 17:58:50 +0300

    Reset Password add

--- src/main/java/tz/go/zanemr/auth/modules/user/UserService.java
+++ src/main/java/tz/go/zanemr/auth/modules/user/UserService.java
@@ -2,6 +2,9 @@ package tz.go.zanemr.auth.modules.user;
 
 import tz.go.zanemr.auth.core.BaseCrudService;
 
+import java.util.UUID;
+
 public interface UserService extends BaseCrudService<UserDto, Long> {
     void changePassword(UserChangePasswordDto userChangePasswordDto);
+    void resetPassword(UUID uuid,UserResetPasswordDto userResetPasswordDto);
 }
